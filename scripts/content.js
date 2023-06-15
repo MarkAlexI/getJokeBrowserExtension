f79ef058ed540188c021d35a4b9fdd1b36ed6759
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-06-16 00:22:33 +0300

    Added popup for displaying answer from api joke.

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,12 @@
 console.log('Work!');
+async function fetchData() {
+  const res = await fetch("https://v2.jokeapi.dev/joke/Any");
+  const record = await res.json();
+  
+  document.getElementById("setup").innerHTML = record.setup;
+  document.getElementById("delivery").innerHTML = record.delivery;
+  
+  console.dir(record);
+}
+
+fetchData();
