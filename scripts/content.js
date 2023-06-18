78d448e815595d606a4f9d67b659fdcb44e038a4
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-06-18 03:29:40 +0300

    Added handling single joke.

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,17 @@
-console.log('Work!');
 async function fetchData() {
   const res = await fetch("https://v2.jokeapi.dev/joke/Any");
   const record = await res.json();
   
-  document.getElementById("setup").innerHTML = record.setup;
-  document.getElementById("delivery").innerHTML = record.delivery;
+  document.getElementById("category").innerHTML = record.category;
+  
+  if (record.type === 'twopart') {
+    document.getElementById("setup").innerHTML = record.setup;
+    document.getElementById("delivery").innerHTML = record.delivery;
+  }
+  
+  if (record.type === 'single') {
+    document.getElementById("joke").innerHTML = record.joke;
+  }
   
   console.dir(record);
 }
